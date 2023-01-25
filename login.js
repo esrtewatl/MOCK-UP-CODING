d3ab2f668a762faf0d6b7c5c5c21728602dc2fcb
Erica Stewart <114890276+esrtewatl@users.noreply.github.com>
2023-01-25 16:45:14 -0500

    login validation working

--- login.js
+++ login.js
@@ -1,58 +1,41 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCjqlUnnQsOydcM4XSX7eO2bh08gndDLQA",
-  authDomain: "login-page-spacebook.firebaseapp.com",
-  projectId: "login-page-spacebook",
-  storageBucket: "login-page-spacebook.appspot.com",
-  messagingSenderId: "501666617517",
-  appId: "1:501666617517:web:96ff7879a0291d453dce24"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-document.getElementById(`submit`).addEventListener(`click`, function(){
-    const loginemail = document.getElementById(`#email`).value;
-    const password = document.getElementById(`#password`).value;
-    signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-})
-    let email = document.getElementById("#email").value;
-    let password = document.getElementById("#password").value;
-    let remember = document.getElementById("#remember").checked;
-
-   
-
-  // Validate password
-  if (passwordValue.length < 8) {
-      // Password too short
-      alert("Password must be at least 8 characters long");
-      
-  }
-
-  // Sign in with email and password
-  firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
-      .then(() => {
-          // Save remember me status in local storage
-          localStorage.setItem("remember", rememberValue);
-          // Redirect to dashboard
-          window.location.replace()("dashboard.html");
-      })
-      .catch(error => {
-          console.log(error);
-          // Handle errors here
-      });
 
 
+let objPeople = [
+    {email:"samR@gmail.com",
+    password:"bullet"},
+    {email:"markT@gmail.com",
+    password:"rockwithus"},
+    {email:"estewartATL@gmail.com",password:"password"}    
+]
+
+let loginForm = document.getElementById("loginform");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let resultBox = document.getElementById("result-box");
+
+// Add form submit event listener
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // Get email and password
+    let emailValue = email.value;
+    let passwordValue = password.value;
+
+    // Validate email and password against objPeople
+    let isValid = false;
+    for (let i = 0; i < objPeople.length; i++) {
+        if (objPeople[i].email === emailValue && objPeople[i].password === passwordValue) {
+            isValid = true;
+            break;
+        }
+    }
+
+    if (isValid) {
+        // Email and password are valid
+        // Redirect to dashboard
+        window.location.href = "dashboard.html";
+    } else {
+        // Invalid email or password
+        resultBox.innerHTML = "Invalid email or password";
+    }
+});
